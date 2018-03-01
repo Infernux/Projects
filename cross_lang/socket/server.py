c323dad4ffe6a46d546f323f33d07e25ebeec074
Mrnux <mrnux@nux.com>
2018-03-01 21:58:56 +0000

    Start implementing a listener thread for all connections

--- cross_lang/socket/server.py
+++ cross_lang/socket/server.py
@@ -3,26 +3,60 @@ import socket
 import struct
 import select
 import sys
+import threading
+import signal
 
 port = 8080
 
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-s.bind(('', port))
+clients = []
+listen_list=[]
 
-s.listen(1)
-client, addr = s.accept()
+class ConnectionListener(threading.Thread):
+    def __init__(self):
+        super(ConnectionListener, self).__init__()
+        self.running = False
 
-print "Someone connected : "+str(addr)
+    def run(self):
+        self.running = True
+        self.s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        self.s.bind(('', port))
+        self.s.settimeout(1)
+
+        while(self.running):
+            self.s.listen(0)
+            try:
+                client, addr = self.s.accept()
+
+                clients.append(client)
+                listen_list.append(client)
+
+                print "Someone connected : "+str(addr)
+            except:
+                pass
+
+    def stop(self):
+        self.running = False
+        self.s.close()
+
+c = ConnectionListener()
+
+def handler(signum, frame):
+    print "signal received"
+    c.stop()
+    #join ?
+
+signal.signal(signal.SIGINT, handler)
+c.start()
 
 BUFFER_SIZE = 1024
 
-listen_list=[]
-listen_list.append(client)
-
 while True:
-    ready_to_read, ready_to_write, error = \
-            select.select(listen_list, [], [])
+    try:
+        ready_to_read, ready_to_write, error = \
+                select.select(listen_list, [], [], 0)
+    except:
+        sys.exit(1)
 
     for socket in ready_to_read:
         try:
@@ -30,6 +64,8 @@ while True:
             if(len(b)==0):
                 print "Disconnected"
                 listen_list.remove(socket)
+                clients.remove(socket)
+                socket.close()
                 break
             length = int(struct.unpack('i', b)[0])
             b=socket.recv(length)
