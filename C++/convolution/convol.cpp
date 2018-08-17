df6a6ad2913a6c9929d303aab717c69d097fb3b6
Mrnux <mrnux@nux.com>
2018-08-17 23:01:24 +0900

    Convert to grayscale (but result different from python)

--- C++/convolution/convol.cpp
+++ C++/convolution/convol.cpp
@@ -4,6 +4,10 @@
 
 #include "CImg.h"
 
+#define Kr 0.299
+#define Kg 0.587
+#define Kb 0.114
+
 using namespace cimg_library;
 using namespace std;
 
@@ -13,6 +17,44 @@ typedef struct s_Filter
   unsigned int width;
   unsigned int height;
 } *Filter;
+
+double* RGBSpaceToYCrCm(CImg<double> img)
+{
+  double* out = new double[sizeof(double) * img.width() * img.height()];
+
+  for(int j=0; j<img.height(); ++j)
+  {
+    for(int i=0; i<img.width(); ++i)
+    {
+      double red = img(i, j, 0);
+      double green = img(i, j, 1);
+      double blue = img(i, j, 2);
+
+      out[i+j*img.width()] = Kr*red + Kg*green + Kb*blue;
+      //double Pb = 0.5f * (blue - Y)/(1-Kb);
+      //double Pr = 0.5f * (red - Y)/(1-Kr);
+    }
+  }
+
+  for(int i=0; i<10; ++i)
+    cout << out[i] << endl;
+
+  return out;
+}
+
+void YCrCmSpaceToRGB(CImg<double>* d, double* img, int padding, int width, int height)
+{
+  for(int y=0 + padding; y<height + padding; ++y)
+  {
+    for(int x=0 + padding; x<width + padding; ++x)
+    {
+      int Y = img[x + y * (width+padding*2)];
+      (*d)(x-padding, y-padding,0) = Y;
+      (*d)(x-padding, y-padding,1) = Y;
+      (*d)(x-padding, y-padding,2) = Y;
+    }
+  }
+}
 
 Filter createEdgeDetectionFilter()
 {
@@ -58,10 +100,9 @@ Filter createSharpeningFilter()
   return f;
 }
 
-double naive_convol(double* img, Filter filter, int x, int y)
+double naive_convol(double* img, Filter filter, int x, int y, int stride)
 {
   double n = 0.f;
-  int stride = 5;
   for(int j=-1; j<2; ++j)
   {
     for(int i=-1; i<2; ++i)
@@ -119,7 +160,7 @@ double* zero_pad(double* mat, int width, int height, int padding)
 
 double* load_img(const char* path)
 {
-  CImg<double> src(path); 
+  CImg<double> src(path);
   return src;
 }
 
@@ -128,19 +169,23 @@ int main(int argc, char** argv)
   Filter filter = createSharpeningFilter();
   //double* img = create_example();
   //double* img = load_img(argv[1]);
-  CImg<double> img(argv[1]); 
+  CImg<double> img(argv[1]);
+  printf("w: %d, h: %d\n", img.width(), img.height());
+
+  double* img2 = RGBSpaceToYCrCm(img);
 
   int padding = 1, width = img.width(), height = img.height();
-  double* padded_img = zero_pad(img,width,height,padding);
+  double* padded_img = zero_pad(img2,width,height,padding);
 
-  for(int y=0 + padding; y<height + padding; ++y)
+  /*for(int y=0 + padding; y<height + padding; ++y)
   {
     for(int x=0 + padding; x<width + padding; ++x)
     {
-      naive_convol(padded_img, filter, x, y);
-      img[(x-padding) + ((y-padding) * height)] = naive_convol(padded_img, filter, x, y);
+      img2[(x-padding) + (y-padding) * width] = naive_convol(padded_img, filter, x, y, (width+padding*2));
     }
-  }
+  }*/
+
+  YCrCmSpaceToRGB(&img, img2, 0, width, height);
 
   img.save("output.bmp");
 
@@ -148,4 +193,5 @@ int main(int argc, char** argv)
   delete[] padded_img;
   //delete[] img;
   delete filter;
+  delete img2;
 }
