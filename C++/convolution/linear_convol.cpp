ca29b8e2247b7c6c98c9194a8cde15dbe5d9e976
Mrnux <mrnux@nux.com>
2018-09-04 00:10:26 +0900

    Fasteeeer (moving the pointer)

--- C++/convolution/linear_convol.cpp
+++ C++/convolution/linear_convol.cpp
@@ -43,35 +43,37 @@ void LinearConvol::convol(double* out, double* img, Filter filter, const unsigne
   double f[9] = { filter->matrix[0], filter->matrix[1], filter->matrix[2], filter->matrix[3], filter->matrix[4], filter->matrix[5], filter->matrix[6], filter->matrix[7], filter->matrix[8] };
 
   int count = 0, A = 0, i;
+  double* trav = img;
   for(int j=0; j<H; j++, A += S)
   {
-    n[count]  = f[1] * img[A      +P  ];
-    n[count] += f[2] * img[A      +P+1];
-    n[count] += f[4] * img[A+S    +P  ];
-    n[count] += f[5] * img[A+S    +P+1];
-    n[count] += f[7] * img[A+S+S  +P  ];
-    n[count] += f[8] * img[A+S+S  +P+1];
+    trav = &img[A+P];
+    n[count]  = f[1] * trav[     0  ];
+    n[count] += f[2] * trav[     1];
+    n[count] += f[4] * trav[S      ];
+    n[count] += f[5] * trav[S    +1];
+    n[count] += f[7] * trav[S+S    ];
+    n[count] += f[8] * trav[S+S  +1];
     ++count;
 
     for(int i=1; i<W-1; ++i, ++count)
     {
-      n[count]  = f[0] * img[A      +P+i-1];
-      n[count] += f[1] * img[A      +P+i  ];
-      n[count] += f[2] * img[A      +P+i+1];
-      n[count] += f[3] * img[A+S    +P+i-1];
-      n[count] += f[4] * img[A+S    +P+i  ];
-      n[count] += f[5] * img[A+S    +P+i+1];
-      n[count] += f[6] * img[A+S+S  +P+i-1];
-      n[count] += f[7] * img[A+S+S  +P+i  ];
-      n[count] += f[8] * img[A+S+S  +P+i+1];
+      n[count]  = f[0] * trav[     i-1];
+      n[count] += f[1] * trav[     i  ];
+      n[count] += f[2] * trav[     i+1];
+      n[count] += f[3] * trav[S    +i-1];
+      n[count] += f[4] * trav[S    +i  ];
+      n[count] += f[5] * trav[S    +i+1];
+      n[count] += f[6] * trav[S+S  +i-1];
+      n[count] += f[7] * trav[S+S  +i  ];
+      n[count] += f[8] * trav[S+S  +i+1];
     }
 
-    n[count]  = f[0] * img[A      +P+W-1-1];
-    n[count] += f[1] * img[A      +P+W-1  ];
-    n[count] += f[3] * img[A+S    +P+W-1-1];
-    n[count] += f[4] * img[A+S    +P+W-1  ];
-    n[count] += f[6] * img[A+S+S  +P+W-1-1];
-    n[count] += f[7] * img[A+S+S  +P+W-1  ];
+    n[count]  = f[0] * trav[     W-1-1];
+    n[count] += f[1] * trav[     W-1  ];
+    n[count] += f[3] * trav[S    +W-1-1];
+    n[count] += f[4] * trav[S    +W-1  ];
+    n[count] += f[6] * trav[S+S  +W-1-1];
+    n[count] += f[7] * trav[S+S  +W-1  ];
     ++count;
   }
 
