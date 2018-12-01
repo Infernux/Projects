bfd1abd97edbbbcc1694043936d9e9e4fc425710
Mrnux <mrnux@nux.com>
2018-12-02 00:01:10 +0900

    Fix AVX (but super slow)

--- C++/convolution/avx_convol.cpp
+++ C++/convolution/avx_convol.cpp
@@ -22,10 +22,49 @@ void AVXConvol::convol(double* out, double* img, Filter filter, const unsigned i
   __m256d f2 = _mm256_set_pd(f[0], f[5], f[4], f[3]);
   __m256d f3 = _mm256_set_pd(f[0], f[8], f[7], f[6]);
 
+  /* compute top line */
+  double el = img[padding + padded_width];
+  n[0]      += el * f[4];
+  n[1]      += el * f[3];
+  n[W]      += el * f[1];
+  n[W + 1]  += el * f[0];
+
+  for(int i=padding + 1; i < padded_width - padding - 1; ++i)
+  {
+    unsigned int i_dest = i - padding;
+    double el = img[padded_width + i];
+
+    n[i_dest - 1]        += el * f[5];
+    n[i_dest]            += el * f[4];
+    n[i_dest + 1]          += el * f[3];
+    n[(1) * W + i_dest - 1]  += el * f[2];
+    n[(1) * W + i_dest]      += el * f[1];
+    n[(1) * W + i_dest + 1]  += el * f[0];
+  }
+
+  el = img[padded_width + padded_width - padding - 1];
+  n[W-2]       += el * f[5];
+  n[W-1]       += el * f[4];
+  n[W + W - 2] += el * f[2];
+  n[W + W - 1] += el * f[1];
+
   for(int j=padding + 1; j<padded_height-padding - 1; ++j)
   {
     unsigned int offset_y_orig = j * padded_width;
     unsigned int j_dest = j - padding;
+    /*
+     */
+    double el = img[offset_y_orig + padding];
+    /*
+     * broadcast to the 6 adjacent elements
+     */
+    n[(j_dest-1) * W]     += el * f[7];
+    n[(j_dest-1) * W + 1] += el * f[6];
+    n[(j_dest) * W]       += el * f[4];
+    n[(j_dest) * W + 1]   += el * f[3];
+    n[(j_dest+1) * W]     += el * f[1];
+    n[(j_dest+1) * W + 1] += el * f[0];
+
     for(int i=padding + 1; i<padded_width-padding - 1; ++i)
     {
       unsigned int i_dest = i - padding;
@@ -48,7 +87,45 @@ void AVXConvol::convol(double* out, double* img, Filter filter, const unsigned i
       n[(j_dest+1)*W + i_dest]    += res[1]; //真ん中
       n[(j_dest+1)*W + i_dest+1]  += res[0]; //右
     }
+
+    el = img[offset_y_orig + padded_width - 2];
+    /*
+     * broadcast to the 6 adjacent elements
+     */
+    n[(j_dest-1) * W + (W - 1) - 1] += el * f[8];
+    n[(j_dest-1) * W + (W - 1)]     += el * f[7];
+    n[(j_dest) * W + (W - 1) - 1]   += el * f[5];
+    n[(j_dest) * W + (W - 1)]       += el * f[4];
+    n[(j_dest+1) * W + (W - 1) - 1] += el * f[2];
+    n[(j_dest+1) * W + (W - 1)]     += el * f[1];
   }
+
+  /* compute bottom line */
+  el = img[(padded_width * (padded_height - 1 - padding)) + padding];
+  n[(H-2)*W]      += el * f[7];
+  n[(H-2)*W+1]    += el * f[6];
+  n[(H-1)*W]      += el * f[4];
+  n[(H-1)*W+1]    += el * f[3];
+
+  unsigned int offset_y_orig = (padded_height - 1 - padding) * padded_width;
+  for(int i=padding + 1; i < padded_width - padding - 1; ++i)
+  {
+    unsigned int i_dest = i - padding;
+    double el = img[offset_y_orig + i];
+
+    n[(H-1 - 1)*W + i_dest -1] += el * f[8];
+    n[(H-1 - 1)*W + i_dest]    += el * f[7];
+    n[(H-1 - 1)*W + i_dest+1]  += el * f[6];
+    n[(H-1)*W + i_dest -1]     += el * f[5];
+    n[(H-1)*W + i_dest]        += el * f[4];
+    n[(H-1)*W + i_dest+1]      += el * f[3];
+  }
+
+  el = img[(padded_width * (padded_height - 1 - padding)) + padded_width - padding - 1];
+  n[(H-2)*W + W - 2]       += el * f[8];
+  n[(H-2)*W + W - 1]       += el * f[7];
+  n[(H-1)*W + W - 2] += el * f[5];
+  n[(H-1)*W + W - 1] += el * f[4];
 
   for(int i=0; i<total_size; ++i)
   {
