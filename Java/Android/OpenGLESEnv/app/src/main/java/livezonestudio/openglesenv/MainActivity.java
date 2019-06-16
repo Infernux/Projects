05808be7661a33872394e6204ba4df13d6c5a51d
Mrnux <mrnux@nux.com>
2019-06-16 18:23:00 +0900

    Working shader. Need cleaning

--- Java/Android/OpenGLESEnv/app/src/main/java/livezonestudio/openglesenv/MainActivity.java
+++ Java/Android/OpenGLESEnv/app/src/main/java/livezonestudio/openglesenv/MainActivity.java
@@ -1,11 +1,9 @@
 package livezonestudio.openglesenv;
 
 import android.content.Context;
-import android.opengl.GLES20;
 import android.opengl.GLES32;
 import android.opengl.GLSurfaceView;
 import android.opengl.Matrix;
-import android.support.v4.content.res.ResourcesCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -22,10 +20,13 @@ import static android.opengl.GLSurfaceView.RENDERMODE_WHEN_DIRTY;
 public class MainActivity extends AppCompatActivity {
     private GLSurfaceView oglsurface;
     private String TAG = "gles";
+    static float time = 0.f;
 
     private CustomOpenGLRenderer renderer;
     int vPosition;
     int uMVPMatrix;
+    int iResolution;
+    int iTime;
     private float[] viewMatrix = new float[16];
     private float[] mvpMatrix = new float[16];
     private float[] projectionMatrix = new float[16];
@@ -34,6 +35,8 @@ public class MainActivity extends AppCompatActivity {
 
     private static String POSITION = "vposition";
     private static String MVP_MATRIX = "mvpmatrix";
+    private static String RESOLUTION = "iResolution";
+    private static String TIME = "iTime";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -44,20 +47,22 @@ public class MainActivity extends AppCompatActivity {
         renderer = new CustomOpenGLRenderer();
 
         oglsurface.setRenderer(renderer);
-        oglsurface.setRenderMode(RENDERMODE_WHEN_DIRTY);
+        //oglsurface.setRenderMode(RENDERMODE_WHEN_DIRTY);
 
         setContentView(oglsurface);
     }
 
 
 
-    private static final String myShader =
-            "varying vec2 position;" +
-             "precision mediump float;" +
-            "#define mode 0" +
-"#define nbPoints 6" +
+    private static final String myShader = "" +
+"precision mediump float;" +
+"\n#define mode 0\n" +
+"\n#define nbPoints 6\n" +
+"uniform float " + TIME + ";" +
+"uniform vec2 "+ RESOLUTION + ";" +
 "void main()" +
 "{" +
+    "vec2 position = gl_FragCoord.xy;" +
     "vec2 seeds[nbPoints];" +
     "seeds[0]=vec2(360.0, 23.2);" +
     "seeds[1]=vec2(80.0, 65.2);" +
@@ -77,51 +82,61 @@ public class MainActivity extends AppCompatActivity {
         "}" +
     "}" +
     "if(mode==1)" +
+    "{" +
     	"closestV=closestV/100.0;" +
+    "}" +
     "if(mode==0){" +
         "if(closest==0)" +
+        "{" +
             "gl_FragColor=vec4(1.0,1.0,1.0,1.0);" +
+        "}" +
         "else if(closest==1)" +
+        "{" +
             "gl_FragColor=vec4(1.0,1.0,0.0,1.0);" +
+        "}" +
         "else if(closest==2)" +
+        "{" +
             "gl_FragColor=vec4(1.0,0.0,1.0,1.0);" +
+        "}" +
         "else if(closest==3)" +
+        "{" +
             "gl_FragColor=vec4(0.0,1.0,1.0,1.0);" +
+        "}" +
         "else if(closest==4)" +
+        "{" +
             "gl_FragColor=vec4(0.0,0.0,1.0,1.0);" +
+        "}" +
         "else" +
+        "{" +
             "gl_FragColor=vec4(0.0,0.0,0.0,0.0);" +
+        "}" +
     "}" +
     "if(mode==1)" +
+    "{" +
     	"gl_FragColor=vec4(closestV,1.0-closestV,0.0,1.0);" +
+    "}" +
     "vec2 uv = position.xy / iResolution.xy;" +
     "for(int i=0; i<nbPoints; ++i){" +
         "if(position.x>=seeds[i].x-10.0 && position.x<=seeds[i].x+10.0 &&" +
            "position.y>=seeds[i].y-10.0 && position.y<=seeds[i].y+10.0){" +
-            "//gl_FragColor=vec4(uv,0.5+0.5*sin(iTime),1.0);" +
-            "gl_FragColor=vec4(1.0,0.0,0.0,1.0);" +
+            "gl_FragColor=vec4(uv,0.5+0.5*sin(iTime),1.0);" +
             "break;" +
         "}" +
     "}" +
 "}";
         private static final String vertexshader = "" +
             "precision mediump float;" +
-"uniform mat4 mvpmatrix;" +
+"uniform mat4 "+ MVP_MATRIX + ";" +
 "attribute vec4 " + POSITION +";" +
+"uniform vec2 "+ RESOLUTION + ";" +
+"uniform float "+ TIME + ";" +
 "varying vec2 position;" +
 "void main() {" +
-  "gl_Position = mvpmatrix * " + POSITION + ";" +
+  "gl_Position = " + MVP_MATRIX + " * " + POSITION + ";" +
             "position = gl_Position.xy;" +
 "}";
 
-    private String fragmentShader = "precision mediump float;" +
-"varying vec2 position;" +
-"void main() {" +
-  "gl_FragColor = vec4(1.0, 0.0, 0.0, 1.0);" +
-"}";
-
     private int program;
-    int position_location;
 
     private class CustomOpenGLSurfaceView extends GLSurfaceView
     {
@@ -139,9 +154,17 @@ public class MainActivity extends AppCompatActivity {
     private FloatBuffer positionBuffer = ByteBuffer.allocateDirect(POSITION_MATRIX.length * 4)
             .order(ByteOrder.nativeOrder()).asFloatBuffer().put(POSITION_MATRIX);
 
+
+
+    private static final float[] RESOLUTION_MATRIX = {
+            480, 1080
+    };
+
+    private FloatBuffer resolutionBuffer = ByteBuffer.allocateDirect(RESOLUTION_MATRIX.length * 4)
+            .order(ByteOrder.nativeOrder()).asFloatBuffer().put(RESOLUTION_MATRIX);
+
     private class CustomOpenGLRenderer implements GLSurfaceView.Renderer
     {
-        private int width, height;
         public int loadShader(int type, String shader)
         {
             int[] compiled = new int[1];
@@ -166,7 +189,7 @@ public class MainActivity extends AppCompatActivity {
 
             int []link = new int[1];
             int vertex_shader_id = loadShader(GLES32.GL_VERTEX_SHADER, vertexshader);
-            int fragment_shader_id = loadShader(GLES32.GL_FRAGMENT_SHADER, fragmentShader);
+            int fragment_shader_id = loadShader(GLES32.GL_FRAGMENT_SHADER, myShader);
 
             program = GLES32.glCreateProgram();
 
@@ -186,12 +209,12 @@ public class MainActivity extends AppCompatActivity {
 
             vPosition = GLES32.glGetAttribLocation(program, POSITION);
             uMVPMatrix = GLES32.glGetUniformLocation(program, MVP_MATRIX);
+            iResolution = GLES32.glGetUniformLocation(program, RESOLUTION);
+            iTime = GLES32.glGetUniformLocation(program, TIME);
         }
 
         @Override
         public void onSurfaceChanged(GL10 gl10, int width, int height) {
-            this.width = width;
-            this.height = height;
             GLES32.glViewport(0, 0, width, height);
 
             float ratio = (float) width / height;
@@ -200,13 +223,15 @@ public class MainActivity extends AppCompatActivity {
             oglsurface.requestRender();
         }
 
+
+
         @Override
         public void onDrawFrame(GL10 gl10) {
             GLES32.glClear(GLES32.GL_COLOR_BUFFER_BIT | GLES32.GL_DEPTH_BUFFER_BIT);
 
             // Using matrices, we set the camera at the center, advanced of 7 looking to the center back
             // of -1
-            Matrix.setLookAtM(viewMatrix, 0, 0, 0, 7, 0, 0, -1, 0, 1, 0);
+            Matrix.setLookAtM(viewMatrix, 0, 0, 0, 4, 0, 0, -1, 0, 1, 0);
             // We combine the scene setup we have done in onSurfaceChanged with the camera setup
             Matrix.multiplyMM(mvpMatrix, 0, projectionMatrix, 0, viewMatrix, 0);
             // We combile that with the applied rotation
@@ -216,15 +241,22 @@ public class MainActivity extends AppCompatActivity {
             // We attach the float array containing our Matrix to the correct handle
             GLES32.glUniformMatrix4fv(uMVPMatrix, 1, false, mvpMatrix, 0);
 
+            resolutionBuffer.position(0);
+            GLES32.glVertexAttribPointer(iResolution, 2, GLES32.GL_FLOAT, false, 0, resolutionBuffer);
+            GLES32.glEnableVertexAttribArray(iResolution);
+
             positionBuffer.position(0);
             GLES32.glVertexAttribPointer(vPosition, 3, GLES32.GL_FLOAT, false, 0, positionBuffer);
             GLES32.glEnableVertexAttribArray(vPosition);
 
+            time += 0.01;
+            GLES32.glUniform1f(iTime, time);
+
             GLES32.glDrawArrays(GLES32.GL_TRIANGLE_STRIP, 0, 4);
 
+            GLES32.glDisableVertexAttribArray(iResolution);
             GLES32.glDisableVertexAttribArray(vPosition);
-
-            //GLES32.glVertexAttribPointer(position_location, 2, GLES20.GL_FLOAT, false, 0, positionBuffer);
+            GLES32.glDisableVertexAttribArray(iTime);
 
         }
     }
