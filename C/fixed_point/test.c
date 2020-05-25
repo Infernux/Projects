6cdccf82e1e1a586546af239886e54e30dd1ba64
mrnux <mrnux@mrnux.com>
2020-05-26 00:00:39 +0900

    Fixed point update

--- C/fixed_point/test.c
+++ C/fixed_point/test.c
@@ -3,7 +3,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#define FIXED_POINT 27
+#define MANTISSE 15
+#define FIXED_POINT (32 - 1 - MANTISSE)
 
 double fixed_to_double(const int32_t number)
 {
@@ -15,7 +16,25 @@ int32_t double_to_fixed(const double number)
   return round(number * (1 << FIXED_POINT));
 }
 
+int32_t fixed_mul(const int32_t number, const int32_t number2) {
+  return ((int64_t)number * (int64_t)number2) >> FIXED_POINT;
+}
+
+int32_t fixed_div(const int32_t number, const int32_t number2) {
+  return (((int64_t)number * (1 << FIXED_POINT)) / number2);
+}
+
 int main()
 {
-  printf("%f a\n", fixed_to_double((double_to_fixed(4.f) >> 16) * (double_to_fixed(2.f) >> 16)<<5));
+  double a = 5.234;
+  double b = 3.234;
+
+  printf("%f*%f -> %f, fixed %f\n",
+      a,b,
+      a*b,
+      fixed_to_double(fixed_mul(double_to_fixed(a), double_to_fixed(b))));
+  printf("%f/%f -> %f, fixed %f\n",
+      a,b,
+      a/b,
+      fixed_to_double(fixed_div(double_to_fixed(a), double_to_fixed(b))));
 }
