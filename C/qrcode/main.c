74583337571f16f19182461cc6365decdc9cc86c
mrnux <mrnux@mrnux.com>
2020-06-14 23:53:31 +0900

    Complete padding. Draw data pixels into the matrix

--- C/qrcode/main.c
+++ C/qrcode/main.c
@@ -11,6 +11,20 @@
 #define ZERO_SET 2
 #define SEPARATOR_SIZE 1
 
+#define PADDING_PATTERN_1 0xEC
+#define PADDING_PATTERN_2 0x11
+
+#define V1_L_CODEWORD_COUNT 19
+#define V1_M_CODEWORD_COUNT 16
+#define V1_Q_CODEWORD_COUNT 13
+#define V1_H_CODEWORD_COUNT 9
+#define V1_L_EC_COUNT 7
+#define V1_M_EC_COUNT 10
+#define V1_Q_EC_COUNT 13
+#define V1_H_EC_COUNT 17
+
+#define min(a,b) (a<b?a:b)
+
 static uint32_t get_significant_bit_count(uint32_t val, uint32_t max_length)
 {
   uint32_t length = max_length;
@@ -243,7 +257,7 @@ static inline uint8_t convertCharToAlphanumeric(const char character) {
 }
 
 /* encode by pairs */
-static void encodeMessageAlphanumeric(const char *string, const uint32_t length, uint8_t *encoded) {
+static uint32_t encodeMessageAlphanumeric(const char *string, const uint32_t length, uint8_t *encoded) {
   uint32_t i = 0;
   uint32_t index = 0;
   for(i = 0; i < length-1; i+=2) {
@@ -251,29 +265,34 @@ static void encodeMessageAlphanumeric(const char *string, const uint32_t length,
     uint8_t char2 = convertCharToAlphanumeric(string[i+1]);
     uint16_t charpair = char1 * 45 + char2; /* 45 is the maximum char */
     for(uint32_t ind = 0; ind < 11; ++ind) {
-      encoded[index] = charpair & (1 << ind) ? 1 : ZERO_SET;
-      index++;
+      encoded[index++] = charpair & (1 << ind) ? 1 : ZERO_SET;
     }
   }
   if(length - index) { /* length % 2 != 0 */
     uint8_t char1 = convertCharToAlphanumeric(string[i]);
     for(uint32_t ind = 0; ind < 6; ++ind) {
-      encoded[index] = char1 & (1 << ind) ? 1 : ZERO_SET;
+      encoded[index++] = char1 & (1 << ind) ? 1 : ZERO_SET;
     }
   }
+
+  return index;
 }
 
-void encodeMessage(const char *string, const uint32_t length, const ENCODING encoding, uint8_t *encoded) {
+void encodeMessage(const char *string, const uint32_t length, const ENCODING encoding, const uint32_t max_len, uint8_t *encoded) {
+  uint32_t bit_count = 0;
   encoded[0] = encoding & 1 ? 1 : ZERO_SET;
   encoded[1] = encoding & 2 ? 1 : ZERO_SET;
   encoded[2] = encoding & 4 ? 1 : ZERO_SET;
   encoded[3] = encoding & 8 ? 1 : ZERO_SET;
+  bit_count = 4;
 
   switch(encoding) {
     case ENCODING_NUMERIC:
       encoded[ENCODING_LEN + 9] = length & (1 << 9) ? 1 : ZERO_SET;
+      bit_count++;
     case ENCODING_ALPHANUMERIC:
       encoded[ENCODING_LEN + 8] = length & (1 << 8) ? 1 : ZERO_SET;
+      bit_count++;
     case ENCODING_BYTE:
     case ENCODING_KANJI:
       encoded[ENCODING_LEN + 7] = length & (1 << 7) ? 1 : ZERO_SET;
@@ -284,13 +303,14 @@ void encodeMessage(const char *string, const uint32_t length, const ENCODING enc
       encoded[ENCODING_LEN + 2] = length & (1 << 2) ? 1 : ZERO_SET;
       encoded[ENCODING_LEN + 1] = length & (1 << 1) ? 1 : ZERO_SET;
       encoded[ENCODING_LEN + 0] = length & (1 << 0) ? 1 : ZERO_SET;
+      bit_count += 8;
   }
 
   switch(encoding) {
     case ENCODING_NUMERIC:
       break;
     case ENCODING_ALPHANUMERIC:
-      encodeMessageAlphanumeric(string, length, &encoded[ENCODING_LEN + 8 + 1]);
+      bit_count += encodeMessageAlphanumeric(string, length, &encoded[ENCODING_LEN + 8 + 1]);
       break;
     case ENCODING_BYTE:
       break;
@@ -302,9 +322,73 @@ void encodeMessage(const char *string, const uint32_t length, const ENCODING enc
       printf("Unknown encoding\n");
       exit(1);
   }
+
+  printf("bit count : %d\n", bit_count);
+
+  /* if there's enough space, add "the" terminator */
+  for(uint32_t i = 0; i < min(4, max_len - bit_count); i++) {
+    encoded[bit_count++] = ZERO_SET;
+  }
+
+  printf("bit count after terminator : %d\n", bit_count);
+
+  /* add zeroes to make it a multiple of 8 */
+  for(uint32_t i = bit_count % 8; i < 8; ++i) {
+    encoded[bit_count++] = ZERO_SET;
+  }
+
+  printf("bit count after 8 rounding : %d\n", bit_count);
+
+  /* pad the remaining codewords */
+  uint32_t remaining_bytes = (max_len - bit_count) / 8;
+  if(remaining_bytes != 0) {
+    for(uint32_t needed_pads = remaining_bytes; needed_pads > 0; --needed_pads) {
+      if((needed_pads % 2)) {
+        for(uint32_t i=0; i<8; i++) {
+          encoded[bit_count++] = PADDING_PATTERN_1 & (1<<i) ? 1 : ZERO_SET;
+        }
+      } else {
+        for(uint32_t i=0; i<8; i++) {
+          encoded[bit_count++] = PADDING_PATTERN_2 & (1<<i) ? 1 : ZERO_SET;
+        }
+      }
+    }
+  }
+  printf("bit count after padding : %d\n", bit_count);
 }
 
-void drawData(uint8_t *buf, const uint8_t *message, const uint32_t length, const uint32_t width) {
+static void drawData(uint8_t *buf, const uint8_t *message, const uint32_t width) {
+  uint32_t index = 0;
+  uint32_t x = 0;
+  for(uint32_t x = 0; x < width - 1; x+=2) {
+    if(x == (width - POSITION_MARKER_SIZE)) {
+      /* skip timing */
+      x++;
+    }
+
+    uint32_t startPoint = 0;
+    if(x % 4) {
+      startPoint = width - x - 1;
+      for(uint32_t y = 0; y < width; y++) {
+        for(uint32_t xind = 0; xind < 2; xind++) {
+          if(buf[startPoint + (y*width) - xind] == 0) {
+            buf[startPoint + (y*width) - xind] = message[index];
+            index++;
+          }
+        }
+      }
+    } else {
+      startPoint = (width * width) - x - 1;
+      for(uint32_t y = 0; y < width; y++) {
+        for(uint32_t xind = 0; xind < 2; xind++) {
+          if(buf[startPoint - (y*width) - xind] == 0) {
+            buf[startPoint - (y*width) - xind] = message[index];
+            index++;
+          }
+        }
+      }
+    }
+  }
 }
 
 int main() {
@@ -314,7 +398,7 @@ int main() {
   drawTiming(qrbuffer, 21, VERSION_1);
 
   uint8_t format[FORMAT_LENGTH] = {0};
-  generateFormat(format, EC_LEVEL_LOW, MASK_HOR_INTERLEAVE);
+  generateFormat(format, EC_LEVEL_Q, MASK_HOR_INTERLEAVE);
   drawFormat(qrbuffer, format, 21);
 
   /*
@@ -322,9 +406,9 @@ int main() {
    * right now, a single output bit is stored into one byte
    * */
   uint8_t message[1024] = {0};
-  encodeMessage("HELLO WORLD", 11, ENCODING_ALPHANUMERIC, message);
+  encodeMessage("HELLO WORLD", 11, ENCODING_ALPHANUMERIC, V1_Q_CODEWORD_COUNT * 8, message);
 
-  drawData(qrbuffer, message, 40, 21);
+  drawData(qrbuffer, message, 21);
 
   saveAsTextPbm("image.ppm", qrbuffer, 21, 21, RESIZE_FACTOR);
   return 0;
