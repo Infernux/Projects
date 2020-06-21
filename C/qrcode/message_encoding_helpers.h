aeb36732fa117515f82ad3afb1bd6ad7e8ff9db8
mrnux <mrnux@mrnux.com>
2020-06-21 23:17:05 +0900

    fixup! Start moving message encoding stuff to a different file

---
+++ C/qrcode/message_encoding_helpers.h
@@ -0,0 +1,8 @@
+#ifndef MESSAGE_ENCODING_HELPERS__
+#define MESSAGE_ENCODING_HELPERS__
+
+#include <inttypes.h>
+
+uint32_t encodeMessageAlphanumeric(const char *string, const uint32_t length, uint8_t *encoded);
+
+#endif /* MESSAGE_ENCODING_HELPERS__ */
