06aeb4ab260c11e771c5d8b6ae2cb11a0d566850
mrnux <mrnux@mrnux.com>
2020-09-21 21:06:16 +0900

    Add naive_idft test and fix PSD test

--- python/signal/test.py
+++ python/signal/test.py
@@ -3,7 +3,7 @@
 import unittest
 
 import numpy as np
-from signal_tools import generate_signal_from_list_of_freq, compute_PSD, naive_dft
+from signal_tools import generate_signal_from_list_of_freq, compute_PSD, naive_dft, naive_idft
 
 delta = 1e-1
 error = 1e-8
@@ -27,6 +27,15 @@ class TestDFT(unittest.TestCase):
             self.assertTrue(abs(ref_fhat[idx].real - test_fhat[idx][0]) < error)
             self.assertTrue(abs(ref_fhat[idx].imag - test_fhat[idx][1]) < error)
 
+    def test_idft(self):
+        # generate data
+        indices_list, sample_list = generate_signal_from_list_of_freq([120, 50], 0, 1, delta)
+        test_fhat = naive_dft(sample_list) #compute dft using my own function
+        f = naive_idft(test_fhat) #compute dft using my own function
+
+        for idx in range(0, len(sample_list)):
+            self.assertTrue(abs(sample_list[idx].real - f[idx][0]) < error)
+
 class TestPSD(unittest.TestCase):
     def test_PSD(self):
         #generate data
@@ -38,7 +47,7 @@ class TestPSD(unittest.TestCase):
         power_list, freq_list = compute_PSD(myfhat, delta)
         PSD = fhat * np.conj(fhat) / len(fhat)
         for idx in range(0, len(power_list)):
-            self.assertTrue(abs(power_list[idx] - PSD[idx]) < error)
+            self.assertTrue(abs(power_list[idx][0] - PSD[idx].real) < error)
 
 if __name__ == '__main__':
     suite = unittest.TestLoader().loadTestsFromTestCase(TestPSD)
