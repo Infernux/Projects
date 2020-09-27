79e171974bcd2cfcf1fecb0fe8b6ff0f7ffc6c46
mrnux <mrnux@mrnux.com>
2020-09-28 00:06:40 +0900

    Fix dft and add a basic fft (real part is roughly correct, imaginary isnt)

--- python/signal/signal_tools.py
+++ python/signal/signal_tools.py
@@ -36,8 +36,6 @@ def naive_idft(samples):
         for sample in samples:
             acc[0] += sample[0] * cos(w*j*k) - sample[1] * sin(w*j*k)
             acc[1] += sample[0] * sin(w*j*k) + sample[1] * cos(w*j*k)
-            #acc[0] += sample * cos(w*j*k)
-            #acc[1] += sample * sin(w*j*k)
             j+=1
 
         acc[0] /= len(samples)
@@ -46,6 +44,36 @@ def naive_idft(samples):
         fhat.append(acc)
 
     return fhat
+
+def improved_dft(samples):
+    f_even = list()
+    f_odd = list()
+    N = len(samples)
+    w = -2*np.pi/N
+    i = 0
+    for _ in range(0, int(len(samples)/2)):
+        f_even.append(samples[i])
+        i+=1
+        f_odd.append(samples[i])
+        i+=1
+
+    fhat_even = naive_dft(f_even)
+    fhat_odd = naive_dft(f_odd)
+    res = list()
+    for i in range(0, int(len(samples)/2)):
+        v = [fhat_even[i][0] + fhat_odd[i][0] * cos(w*i), fhat_even[i][1] + fhat_odd[i][1] * sin(w*i)]
+        res.append(v)
+    for i in range(0, int(len(samples)/2)):
+        v = [fhat_even[i][0] + fhat_odd[i][0] * cos(w*(i+N/2)), fhat_odd[i][1] * sin(w*(i+N/2))]
+        res.append(v)
+
+    return res
+
+def naive_fft(samples):
+    pass
+
+def naive_ifft(samples):
+    pass
 
 def generate_signal_from_list_of_freq(freq_list, range_from, range_to, delta):
     if range_from > range_to:
@@ -108,16 +136,12 @@ def multiply_complex_by_conjugate(sample):
     # multiplying both complex parts gives i^2 which is -1
     return sample[0] * (conj[0] + conj[1]) + sample[1] * conj[0] - sample[1] * conj[1]
 
-def multiply_complex_by_conjugate_2(sample):
-    # multiplying both complex parts gives i^2 which is -1
-    return sample * sample.conjugate()
-
 def compute_PSD(samples, dt):
     freq_list = list()
     power_list = list()
     i = 0
     for sample in samples:
-        power_list.append(multiply_complex_by_conjugate(sample) / len(samples))
+        power_list.append([multiply_complex_by_conjugate(sample) / len(samples), 0])
         freq_list.append(1/(dt*len(samples)) * i)
         i+=1
 
@@ -132,33 +156,40 @@ if __name__ == '__main__':
     print('main')
     indices_list, sample_list = generate_signal_from_list_of_freq([120, 50], 0, 1, delta)
     noisy_sample_list = add_noise_to_graph(sample_list, 10)
+    for i in range(0, 24):
+        sample_list.append(0)
 
-    #fhat = np.fft.fft(sample_list, len(sample_list))
     #fhat = np.fft.fft(noisy_sample_list, len(sample_list))
-    #fhat = naive_dft(sample_list)
-    fhat = naive_dft(noisy_sample_list)
+    fhat = naive_dft(sample_list)
+    fhat2 = improved_dft(sample_list)
+    print(fhat[0], fhat2[0])
+    print(fhat[1], fhat2[1])
+    print(fhat[2], fhat2[2])
+    #fhat = naive_dft(noisy_sample_list)
+    import sys
+    sys.exit(0)
 
     power_list, freq_list = compute_PSD(fhat, delta)
     max_indice = int(np.floor(len(freq_list)/2))
     freq = (1/(delta*len(fhat))) * np.arange(len(fhat))
-    plt.plot(freq_list, power_list)
+    plt.plot(freq_list, [power[0] for power in power_list])
     plt.xlim(freq_list[1], freq_list[max_indice])
-    plt.ylim(0, max(power_list[1:]))
+    plt.ylim(0, max([power[0] for power in power_list][1:]))
     plt.show()
-    for a in range(0, len(power_list)):
-        power_list[a] = 0 if power_list[a] < 120 else power_list[a]
+    #for a in range(0, len(power_list)):
+    #    power_list[a][0] = 0 if power_list[a][0] < 120 else power_list[a][0]
     #for a in range(100, len(power_list)):
     #    power_list[a] = 0
-    plt.plot(freq_list, power_list)
+    #plt.plot(freq_list, [power[0] for power in power_list])
+    plt.plot(freq_list, [power[0] for power in power_list])
     plt.xlim(freq_list[1], freq_list[max_indice])
-    plt.ylim(0, max(power_list[1:]))
+    plt.ylim(0, max([power[0] for power in power_list][1:]))
     plt.show()
 
-    ifft = np.fft.ifft(fhat)
-    idft = naive_idft(fhat)
+    idft = naive_idft(power_list)
 
     # actually only half the sampling rate is usable
+    plt.plot(indices_list, [item[0] for item in idft], color='b')
     plt.plot(indices_list, sample_list, color='r')
-    plt.plot(indices_list, ifft, color='b')
-    plt.xlim(indices_list[1], indices_list[-1])
+    plt.xlim(indices_list[0], indices_list[-1])
     plt.show()
