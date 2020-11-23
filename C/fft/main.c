e1de293d07cf594b67e75a6f369d1d4330d0c6e0
mrnux <mrnux@mrnux.com>
2020-11-23 20:34:26 +0900

    Working C fft

--- C/fft/main.c
+++ C/fft/main.c
@@ -9,17 +9,17 @@
 #define PI 3.141592
 #define OMEGA_MATRIX_SIZE 8
 
-#define PRINT(a) printf("[%d] : %f+%fi\n", i, omega_matrix[i].real, omega_matrix[i].complex);
+#define PRINT(a) printf("[%d] : %f+%fi\n", i, a.real, a.complex);
 
 typedef struct Number_ {
-  float real;
-  float complex;
+  double real;
+  double complex;
 } Number;
 
 Number* generate_omega_matrix(const uint32_t matrix_size) {
   Number *omega_matrix = (Number*)malloc(sizeof(Number) * matrix_size * matrix_size);
 
-  float w = -2 * PI / (float)matrix_size;
+  double w = -2 * PI / (double)matrix_size;
   for(uint32_t j=0; j < matrix_size; ++j) {
     for(uint32_t k=0; k < matrix_size; ++k) {
       omega_matrix[j*matrix_size + k].real = cos(w * j * k);
@@ -31,7 +31,7 @@ Number* generate_omega_matrix(const uint32_t matrix_size) {
 }
 
 /* mallocs the returned array */
-float* generate_signal_from_list_of_freq(const float *freq_list, const uint32_t freq_count, const float from, const float to, const float delta) {
+Number* generate_signal_from_list_of_freq(const double *freq_list, const uint32_t freq_count, const double from, const double to, const double delta, const uint8_t needs_padding, uint32_t *count, uint32_t *padded_count) {
   if(from > to) {
     printf("From is bigger than to\n");
     return NULL;
@@ -41,18 +41,28 @@ float* generate_signal_from_list_of_freq(const float *freq_list, const uint32_t 
     return NULL;
   }
 
-  float diff = to - from;
-  float t = 0.f;
-  uint32_t count = (uint32_t)(diff / delta);
+  double diff = to - from;
+  double t = 0.f;
+  *count = (uint32_t)(diff / delta);
 
-  float *out = (float*)malloc(sizeof(float) * count);
-  printf("count %d\n", count);
-  for(uint32_t i = 0; i < count; ++i) {
-    float acc = 0.f;
+  printf("data_count %d\n", *count);
+  if(needs_padding) {
+    uint32_t floor_size = ((uint32_t)log2(*count));
+    *padded_count = 1 << (floor_size+1);
+    printf("padding: %d\n", *padded_count);
+  } else {
+    *padded_count = *count;
+  }
+
+  Number *out = (Number*)malloc(sizeof(Number) * *padded_count);
+
+  for(uint32_t i = 0; i < *count; ++i) {
+    double acc = 0.f;
     for(uint32_t freq_idx = 0; freq_idx < freq_count; ++freq_idx) {
       acc += sin(2*PI*freq_list[freq_idx]*t);
     }
-    out[i] = acc;
+    out[i].real = acc;
+    out[i].complex = 0;
     t += delta;
   }
 
@@ -93,51 +103,26 @@ void rearrange_data_for_fft_mem(const Number *in, const uint32_t count, Number *
   }
 }
 
+Number* naive_fft(Number *samples, const uint32_t samples_count, const Number *omega_matrix, const uint32_t omega_matrix_size, Number *inter_buffer) {
+  rearrange_data_for_fft_mem(samples, samples_count, inter_buffer);
 
-#if 0
-Number* naive_fft(const Number *samples, const uint32_t samples_count, const Number *omega_matrix, const uint32_t omega_matrix_size) {
-  if(samples_count <= omega_matrix_size) {
-    return multiply_add_omega_matrix_with_vector(samples, samples_count, omega_matrix, omega_matrix_size);
-  }
-}
-#endif
-
-//#define V 64
-//#define V 65536
-//#define V 262144
-//#define V 2097152
-#define V 4194304 //2^22
-
-int main() {
-  /* init */
-  Number *omega_matrix = generate_omega_matrix(OMEGA_MATRIX_SIZE);
-
-  Number *a=malloc(sizeof(Number) * V);
-  Number *b=malloc(sizeof(Number) * V);
-  for(int i=0; i<V; ++i) {
-    a[i].real = i;
-    a[i].complex = 0;
+  for(int i=0; i < (samples_count/omega_matrix_size); ++i) {
+    uint32_t offset = omega_matrix_size * i;
+    multiply_add_omega_matrix_with_vector(omega_matrix, omega_matrix_size, &inter_buffer[offset], omega_matrix_size);
   }
 
-  rearrange_data_for_fft_mem(a, V, b);
-
-  for(int i=0; i < (V/OMEGA_MATRIX_SIZE); ++i) {
-    uint32_t offset = OMEGA_MATRIX_SIZE * i;
-    multiply_add_omega_matrix_with_vector(omega_matrix, OMEGA_MATRIX_SIZE, &b[offset], OMEGA_MATRIX_SIZE);
-  }
-
-  uint32_t omega_size_log2 = (uint32_t)log2(OMEGA_MATRIX_SIZE);
-  uint32_t data_size_log2  = (uint32_t)log2(V);
+  uint32_t omega_size_log2 = (uint32_t)log2(omega_matrix_size);
+  uint32_t data_size_log2  = (uint32_t)log2(samples_count);
 
   uint32_t iteration_count = data_size_log2 - omega_size_log2;
-  uint32_t half_N = OMEGA_MATRIX_SIZE;
+  uint32_t half_N = omega_matrix_size;
 
-  Number *tmp = a;
-  Number *vals = b;
+  Number *tmp = samples;
+  Number *vals = inter_buffer;
 
   for(uint32_t i = 0; i < iteration_count; ++i) {
-    float w = (-2. * PI) / (float)(half_N*2.);
-    for(uint32_t y = 0; y < (V / half_N / 2); ++y) {
+    double w = (-2. * PI) / (double)(half_N*2.);
+    for(uint32_t y = 0; y < (samples_count / half_N / 2); ++y) {
       Number *even = &vals[(y*2)*half_N];
       Number *odd  = &vals[((y*2)+1)*half_N];
 
@@ -155,21 +140,36 @@ int main() {
     half_N *= 2;
   }
 
-  free(a);
-  free(b);
+  return vals;
+}
+
+#define V 64
+//#define V 65536
+//#define V 262144
+//#define V 2097152
+//#define V 4194304 //2^22
+//#define V 33554432
+
+int main() {
+  /* init */
+  Number *omega_matrix = generate_omega_matrix(OMEGA_MATRIX_SIZE);
+
+  double freq_list[5] = {50.f, 120.f, 192.f, 1200.f, 4000.f};
+
+  uint32_t count = 0, padded_count = 0;
+  Number *sample_list = generate_signal_from_list_of_freq(freq_list, 5, 0, 1, DELTA, 1, &count, &padded_count);
+
+  Number *b=malloc(sizeof(Number) * padded_count);
+
+  Number *res = naive_fft(sample_list, padded_count, omega_matrix, OMEGA_MATRIX_SIZE, b);
+
+  for(int i = 0; i < count; ++i) {
+    PRINT(res[i]);
+  }
+
+  free(sample_list);
   free(omega_matrix);
-
-  return;
-
-  //float freq_list[5] = {50.f, 120.f, 192.f, 1200.f, 4000.f};
-
-  //float *sample_list = generate_signal_from_list_of_freq(freq_list, 5, 0, 1, DELTA);
-
-  //naive_fft(sample_list, sample_count, omega_matrix, OMEGA_MATRIX_SIZE);
-
-  //printf("%f\n", sample_list[1]);
-
-  //free(sample_list);
+  free(b);
 
   return 0;
 }
